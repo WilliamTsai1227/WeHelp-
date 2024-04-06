6bcdfe83df2ee8505c6921aa2426fd617ed7cae6
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-04-07 00:33:08 +0800

    Update index.js

--- week1/index.js
+++ week1/index.js
@@ -14,14 +14,15 @@ function closelist(){
 
 function checkWidth() {
     let windowWidth = window.innerWidth;
-    let op = document.querySelector(".openlist");
+    let op = document.querySelector(".openlist");  //找到現在list狀態為打開的
     let openMu = document.querySelector(".menu_close");
-    if (windowWidth > 600) {
-        console.log("页面宽度大于 600px，执行相应的操作，避免list尚未關掉防呆");
-        op.className="closelist"; //第一和第二畫面皆為display:none,手機畫面按漢堡按鈕才會改變class為openlist
+    if (windowWidth > 600 && op) {                    //當視窗大於600且找到現在list狀態為打開的，會執行以下
+        console.log("頁面寬度大於 600px，執行相應操作，避免list尚未關掉防呆");
+        op.className="closelist"; //第一和第二畫面皆為display:none,更改list 為 closelist是可以關掉list的
         openMu.className = "menu_icon"; //第一和第二畫面皆為display:none,手機畫面才會dispaly:flex
     } 
 }
+
 // 在頁面加載完成時調用
 window.onload = checkWidth;
 
