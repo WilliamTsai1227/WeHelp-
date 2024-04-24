c9c853fa553429ed8df1d04fa6cd0f0b227d7f00
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-04-24 23:43:11 +0800

    Add files via upload

---
+++ week4/app.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse,HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+app.add_middleware(SessionMiddleware, secret_key="secret")
+
+
+@app.get("/")
+async def home(request: Request,response_class=HTMLResponse):
+    request.session["SIGNED-IN"] = False
+    return templates.TemplateResponse("login_page.html",{"request": request})
+
+
+@app.post("/signin")
+async def signin(request: Request,account: str= Form(None), password: str= Form(None)):
+    if account is None or password is None:
+        error_message = "請輸入帳號、密碼"
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
+    elif account != "test" or password != "test":
+        error_message = "帳號、或密碼輸入錯誤"
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
+    else:
+        request.session["SIGNED-IN"] = True
+        return RedirectResponse(url="/member", status_code=303)
+
+
+
+@app.get("/member")
+async def member(request: Request,response_class=HTMLResponse):
+    if "SIGNED-IN" not in request.session:
+        request.session["SIGNED-IN"] = False
+    if request.session["SIGNED-IN"] != True:
+        return RedirectResponse(url="/", status_code=303,)
+    else:
+        return templates.TemplateResponse("success_page.html", {"request": request})
+
+
+
+@app.get("/error")
+async def error(request: Request,message: str=None,response_class=HTMLResponse):
+    request.session["SIGNED-IN"] = False
+    if message == "請輸入帳號、密碼":
+        return templates.TemplateResponse("error_page.html",{"error_message": message ,"request": request})
+    elif message == "帳號、或密碼輸入錯誤":
+        return templates.TemplateResponse("error_page.html",{"error_message": message,"request": request})
+    else:
+        return RedirectResponse(url="/", status_code=303,)
+
+
+
+@app.get("/logout")
+async def logout(request: Request):
+    request.session["SIGNED-IN"] = False
+    return RedirectResponse(url="/", status_code=303)
+
+
+    
+
