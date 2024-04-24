69f96a1b11acbb36dc408e50eb961ce157ada5a3
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-04-24 18:17:46 +0800

    Add files via upload

--- week4/task1.py
+++ week4/task1.py
@@ -1,39 +1,46 @@
-from fastapi import FastAPI, Request,Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+app.add_middleware(SessionMiddleware, secret_key="secret")
 
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("login_page.html",{"request": request})
 
 @app.post("/signin")
-async def signin(account: str= Form(...), password: str= Form(...), checkbox: bool= Form(...)):
-    if not account or not password:
-        error_message = "請輸入帳號和密碼"
-        return RedirectResponse(url=f"/error?message={error_message}")
+async def signin(request: Request,account: str= Form(None), password: str= Form(None)):
+    if account is None or password is None:
+        error_message = "Please_enter_username_and_password"
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
     elif account != "test" or password != "test":
-        error_message = "帳號或密碼不正確"
-        return RedirectResponse(url=f"/error?message={error_message}")
+        error_message = "Username_or_password_is_not_correct"
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
     else:
-        return RedirectResponse(url="/member")
+        request.session["SIGNED-IN"] = True
+        return RedirectResponse(url="/member", status_code=303)
 
-@app.post("/member")
-async def member():
-    return RedirectResponse(url="http://127.0.0.1:8000/member")
+@app.get("/member")
+async def member(request: Request):
+    if not request.session.get("SIGNED-IN"):
+        return RedirectResponse(url="/", status_code=303)
+    return templates.TemplateResponse("success_page.html", {"request": request})
 
 @app.get("/error")
-async def error(message: str):
-    if message == "請輸入帳號和密碼":
+async def error(message: str,request: Request):
+    if message == "Please_enter_username_and_password":
         error_message = "請輸入帳號和密碼"
-    elif message == "帳號或密碼不正確":
-        error_message = "帳號或密碼不正確"
+        return templates.TemplateResponse("error_page.html",{"error_message": error_message ,"request": request})
+    elif message == "Username_or_password_is_not_correct":
+        error_message = "帳號、或密碼輸入錯誤"
+        return templates.TemplateResponse("error_page.html",{"error_message": error_message,"request": request})
     else:
-        error_message = "未知錯誤"
-    return {"error_message": error_message}
+        error_message = message
+        return templates.TemplateResponse("error_page.html",{"error_message": error_message,"request": request})
+    
 
