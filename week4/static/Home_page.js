85c5c010668dc67426c80dd37d3db61082c9be16
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-04-25 11:09:15 +0800

    Add files via upload

---
+++ week4/static/Home_page.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let loginButton = document.querySelector("#login_button");
+    let checkBox = document.querySelector("#check_box input[type='checkbox']");
+    loginButton.addEventListener("click", function(event) {
+        if (!checkBox.checked) {
+            event.preventDefault(); 
+            alert("Please check the checkbox first.");
+        }
+    });
+});
