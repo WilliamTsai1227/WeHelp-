76e7bf0f66efa6faf106b354ffa4a9b2f602a7ec
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-04-25 10:06:05 +0800

    Add files via upload

--- week4/app.py
+++ week4/app.py
@@ -12,18 +12,19 @@ app.add_middleware(SessionMiddleware, secret_key="secret")
 
 
 @app.get("/")
-async def home(request: Request,response_class=HTMLResponse):
-    request.session["SIGNED-IN"] = False
-    return templates.TemplateResponse("login_page.html",{"request": request})
+async def home(request: Request):
+    return templates.TemplateResponse("home_page.html",{"request": request})
 
 
 @app.post("/signin")
 async def signin(request: Request,account: str= Form(None), password: str= Form(None)):
     if account is None or password is None:
         error_message = "請輸入帳號、密碼"
+        request.session["SIGNED-IN"] = False
         return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
     elif account != "test" or password != "test":
         error_message = "帳號、或密碼輸入錯誤"
+        request.session["SIGNED-IN"] = False
         return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
     else:
         request.session["SIGNED-IN"] = True
@@ -32,10 +33,8 @@ async def signin(request: Request,account: str= Form(None), password: str= Form(
 
 
 @app.get("/member")
-async def member(request: Request,response_class=HTMLResponse):
-    if "SIGNED-IN" not in request.session:
-        request.session["SIGNED-IN"] = False
-    if request.session["SIGNED-IN"] != True:
+async def member(request: Request):    
+    if "SIGNED-IN" not in request.session or request.session["SIGNED-IN"] != True :
         return RedirectResponse(url="/")
     else:
         return templates.TemplateResponse("success_page.html", {"request": request})
@@ -43,8 +42,7 @@ async def member(request: Request,response_class=HTMLResponse):
 
 
 @app.get("/error")
-async def error(request: Request,message: str=None,response_class=HTMLResponse):
-    request.session["SIGNED-IN"] = False
+async def error(request: Request,message: str=None):
     if message == "請輸入帳號、密碼":
         return templates.TemplateResponse("error_page.html",{"error_message": message ,"request": request})
     elif message == "帳號、或密碼輸入錯誤":
@@ -53,8 +51,7 @@ async def error(request: Request,message: str=None,response_class=HTMLResponse):
         return RedirectResponse(url="/")
 
 
-
-@app.get("/logout")
+@app.get("/signout")
 async def logout(request: Request):
     request.session["SIGNED-IN"] = False
     return RedirectResponse(url="/")
