8e5a1020330abfab29726fd133119ee5c04f960e
WilliamTsai1227 <160149504+WilliamTsai1227@users.noreply.github.com>
2024-05-23 08:25:03 +0800

    Add files via upload

--- week8/topic_project/app.py
+++ week8/topic_project/app.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 import mysql.connector
 from pydantic import BaseModel
+import re
 
 
 
@@ -15,6 +16,28 @@ templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(SessionMiddleware, secret_key="secret")
 
+# verify input is empty or not
+def verify_NotEmpty_input(name,account,password):
+    if name is None or account is None or password is None:
+        return False
+    return True
+#verify password format and length
+def verify_password(password,min_length,max_length):
+    if len(password)< min_length or len(password)>max_length:
+        return False
+    if re.search(r'/[a-z]/',password) is None:
+        print("a")
+        return False
+    if re.search(r'/[A-Z]/',password) is None:
+        print("b")
+        return False
+    if re.search(r'/\d/',password) is None:
+        print("c")
+        return False
+    if re.search(r'/[@#$%]/',password) is None:
+        print("d")
+        return False
+    return True
 
 @app.get("/")
 async def home(request: Request):
@@ -25,28 +48,35 @@ async def home(request: Request):
     
 @app.post("/signup")
 async def signup(request: Request,register_name: str= Form(None),register_account: str= Form(None), register_password: str= Form(None)):
-    con = mysql.connector.connect(
-        user = "root",
-        password = "12345678",
-        host = "localhost",
-        database = "website"
-    )
-    name = register_name
-    account = register_account
-    password = register_password
-    
-    #創建cursor物件
-    cursor = con.cursor()
-    cursor.execute("SELECT username FROM member WHERE username=%s",(account,)) #尋找相同帳號
-    data = cursor.fetchall()
-    if data == []: #沒找到
-        cursor.execute("INSERT INTO member(name,username,password) VALUES (%s,%s,%s)",(name,account,password))
-        con.commit()
-        con.close()
-        return RedirectResponse(url="/", status_code=303)
-    if data != []: #找到相同帳號
-        con.close()
-        error_message = "Repeated username"
+    #verify input is empty or not
+    #verify password length and format
+    #All pass will enter the database procedure.
+    if verify_NotEmpty_input(register_name,register_account,register_password) and verify_password(register_password,4,8):
+        con = mysql.connector.connect(
+            user = "root",
+            password = "12345678",
+            host = "localhost",
+            database = "website"
+        )
+        name = register_name
+        account = register_account
+        password = register_password
+        
+        #創建cursor物件
+        cursor = con.cursor()
+        cursor.execute("SELECT username FROM member WHERE username=%s",(account,)) #尋找相同帳號
+        data = cursor.fetchall()
+        if data == []: #沒找到
+            cursor.execute("INSERT INTO member(name,username,password) VALUES (%s,%s,%s)",(name,account,password))
+            con.commit()
+            con.close()
+            return RedirectResponse(url="/", status_code=303)
+        if data != []: #找到相同帳號
+            con.close()
+            error_message = "Repeated username"
+            return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
+    else:
+        error_message = "註冊失敗"
         return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
 
     
@@ -55,23 +85,31 @@ async def signup(request: Request,register_name: str= Form(None),register_accoun
 
 @app.post("/signin")
 async def signin(request: Request,account: str= Form(None), password: str= Form(None)):
-    con = mysql.connector.connect(
-        user = "root",
-        password = "12345678",
-        host = "localhost",
-        database = "website"
-    )
-    cursor = con.cursor()
-    cursor.execute("SELECT id, name, username, password FROM member WHERE username=%s AND password=%s",(account,password))
-    data = cursor.fetchone()
-    con.close()
-    if data == None:
-        error_message = "帳號或密碼輸入錯誤"
-        request.session["SIGNED-IN"] = False
+    #verify input is empty or not
+    #verify password length and format 
+    #All pass will enter the database procedure.
+    name = "default"
+    if verify_NotEmpty_input(name,account,password) and verify_password(password,4,8):
+        con = mysql.connector.connect(
+            user = "root",
+            password = "12345678",
+            host = "localhost",
+            database = "website"
+        )
+        cursor = con.cursor()
+        cursor.execute("SELECT id, name, username, password FROM member WHERE username=%s AND password=%s",(account,password))
+        data = cursor.fetchone()
+        con.close()
+        if data == None:
+            error_message = "帳號或密碼輸入錯誤"
+            request.session["SIGNED-IN"] = False
+            return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
+        if account == data[2] and password == data[3]:     
+            request.session.update({"SIGNED-IN": True, "id": data[0], "name": data[1], "username":data[2]})
+            return RedirectResponse(url="/member", status_code=303)
+    else:
+        error_message = "驗證失敗"
         return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
-    if account == data[2] and password == data[3]:     
-        request.session.update({"SIGNED-IN": True, "id": data[0], "name": data[1], "username":data[2]})
-        return RedirectResponse(url="/member", status_code=303)
 
 
 
